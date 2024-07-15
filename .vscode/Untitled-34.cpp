1548cd885d97757aa262c473d63bc143eac8a424
kartik <kartiksinghchauhan2@gmail.com>
2024-07-15 16:40:17 +0530

    code

--- .vscode/Untitled-34.cpp
+++ .vscode/Untitled-34.cpp
@@ -1,12 +1,13 @@
-#include <stdio.h>
+#include <iostream>
+using namespace std;
 int main() {
    int  i, j, n= 1;
-   for (i = 1; i <= 5; i++) {
+   for (i = 1; i <= 4; i++) {
       for (j = 1; j<= i; j++) {
-         printf("%d ", n);
+         cout<<" "<<n;
          n++;
       }
-      printf("\n");
+      cout<<"\n";
    }
    return 0;
 }
