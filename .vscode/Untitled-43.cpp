c386b8c02a415ef4692221da03e814db615a2701
kartik <kartiksinghchauhan2@gmail.com>
2024-07-16 23:22:29 +0530

    code

---
+++ .vscode/Untitled-43.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int marks;
+
+    cout << "Enter your marks: ";
+    cin >> marks;
+
+    if (marks > 100 || marks < 0) {
+        cout << "Invalid marks entered." << endl;
+         
+    }
+
+    switch (marks/10 ) {
+        case 9:
+            cout << "Grade: A+" << endl;
+            break;
+        case 8:
+            cout << "Grade: A" << endl;
+            break;
+        case 7:
+            cout << "Grade: B" << endl;
+            break;
+        case 6:
+            cout << "Grade: C" << endl;
+            break;
+        case 5:
+            cout << "Grade: D" << endl;
+            break;
+        default:
+            cout << "Grade: F" << endl;
+    }
+
+    return 0;
+}
