d2d82cc1ef3fa5da8d86ac04eada003674501714
kartik <kartiksinghchauhan2@gmail.com>
2024-07-17 17:48:07 +0530

    code

---
+++ .vscode/Untiled-46.cpp
@@ -0,0 +1,14 @@
+
+#include <iostream>
+using namespace std;
+int NA(int N){
+    for(int i=1;i<=N;i++){
+        cout<<i<<" ";
+    }
+}
+    int main(){
+    int c;
+    cout<<"Enter the value: ";
+    cin>>c;
+    NA(c);
+}
